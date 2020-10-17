d5147874351ed6b65af9170da0c23265283cdf32
usnabeel1 <73013767+usnabeel1@users.noreply.github.com>
2020-10-17 13:32:39 +0300

    Update Family.java

--- src/com/company/Family.java
+++ src/com/company/Family.java
@@ -4,11 +4,13 @@ public class Family {
 
     private String Firstname;
     private String LastName;
+    private String MiddleName;
 
-    public Family(String firstname, String lastName)
+    public Family(String firstname, String lastName, String MiddleName)
     {
         this.Firstname = firstname;
         this.LastName = lastName;
+        this.MiddleName = MiddleName;
     }
 
     public String Get_FirstName()
@@ -20,4 +22,8 @@ public class Family {
     {
         return LastName;
     }
-}
+    public String Get_MiddleName()
+    {
+        return MiddleName;
+    }
+}// usnabeel1 edited and added Methods for MiddleName
